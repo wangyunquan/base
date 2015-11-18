c09368ab6919a3c8786c74a4c5cc2297022c5079
wangyunquan <wangyunquan@live.com>
2015-11-19 00:52:12 +0800

    dd

---
+++ base/src/test/java/com/buswe/file/search/StringTest.java
@@ -0,0 +1,13 @@
+package com.buswe.file.search;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String []  st="68.116.5.134,24.43.1.206".split(",");
+for(String aa:st)
+{
+	System.out.println(aa);}
+	}
+
+}
